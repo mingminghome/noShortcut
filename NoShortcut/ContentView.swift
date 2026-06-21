70662c653ba04035949858f1e555e73dc8026278
MingMingHomeWork <31679142+mingminghome@users.noreply.github.com>
2026-06-21 22:24:03 +0100

    Initial Commit

---
+++ NoShortcut/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NoShortcut
+//
+//  Created by Ming on 21/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NoShortcut/NoShortcutApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoShortcutApp.swift
+//  NoShortcut
+//
+//  Created by Ming on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NoShortcutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
